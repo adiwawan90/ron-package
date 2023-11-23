5d352ff747a75d22b0d4a938b1e23a61bc3dd0a6
Adi Setyawan <44990911+adiwawan90@users.noreply.github.com>
2023-11-23 13:31:06 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   reactStrictMode: true,
-//   eslint: {
-//     // Warning: This allows production builds to successfully complete even if
-//     // your project has ESLint errors.
-//     ignoreDuringBuilds: true,
-//   },
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
 //   // images: {
 //   //   domains: ["firebasestorage.googleapis.com"],
 //   //   unoptimized: true,
@@ -21,8 +21,13 @@
  */
 const nextConfig = {
   /* config options here */
-  basePath: '/github-pages',
   reactStrictMode: true,
+  basePath: '/github-pages',
+    eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
  
 module.exports = nextConfig
