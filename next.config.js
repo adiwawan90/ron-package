4dae6d151492fdb8d041269557d208f3e1d1fc6d
Adi Setyawan <44990911+adiwawan90@users.noreply.github.com>
2023-11-23 14:04:27 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -21,13 +21,11 @@
  */
 const nextConfig = {
   /* config options here */
-  reactStrictMode: true,
   basePath: '/github-pages',
-    eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 }
  
-module.exports = nextConfig
+module.exports = nextConfig;
