663812ef21fbd5287b9bc154b09fed7ee85291ce
Adi Setyawan <44990911+adiwawan90@users.noreply.github.com>
2023-11-29 11:41:51 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -22,14 +22,14 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
-  basePath: '/github-pages',
+  // basePath: '/github-pages',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true,
+    // unoptimized: true,
   },
 }
  
